ba54038ec9c001524937290091b2802e1e7fe458
viktorcus <carolynseglem@gmail.com>
2023-02-02 11:15:06 -0600

    Defined Student and Grade types. Created Student controller and model files for future use.

---
+++ src/types/studentTypes.d.ts
@@ -0,0 +1,37 @@
+type CourseGrade = {
+    name: string,
+    weight: number,
+    grade: number,
+};
+
+type CourseGrades = {
+    assignmentWeights: Array<CourseGrade>,
+    finalExamWeight: { weight: number },
+};
+
+type Student = {
+    name: string,
+    weights: CourseGrades,
+    currentAverage: number,
+};
+
+type NewStudentRequest = {
+    name: string,
+    weights: CourseGrades,
+};
+
+type AssignmentGrade = {
+    grade: number,
+};
+
+type FinalGrade = {
+    overallScore: number,
+    letterGrade: string,
+};
+
+type FinalExamScores = {
+    neededForA: number,
+    neededForB: number,
+    neededForC: number,
+    neededForD: number,
+}
